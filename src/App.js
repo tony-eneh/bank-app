4c5075bc7bfb1d4bd6fac243eaf54f0afb7ee66c
Tony Eneh <aeneh90@gmail.com>
2020-10-24 12:20:00 +0100

    Feat: dispatch actions on events

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import formatNumber from "format-number";
 import photographer from "./images/girl.png";
 import "./App.css";
 import { store } from "./store";
+import { withdrawMoneyAction, giveAllToCharityAction } from "./actions";
 
 class App extends Component {
   render() {
     const { totalAmount, username } = store.getState();
+
     return (
       <div className="App">
         <img className="App__userpic" src={photographer} alt="user" />
@@ -17,11 +19,30 @@ class App extends Component {
         </div>
 
         <section className="App__buttons">
-          <button data-amount="10000">WITHDRAW $10,000</button>
-          <button data-amount="5000">WITHDRAW $5,000</button>
+          <button
+            data-amount="10000"
+            onClick={(event) =>
+              store.dispatch(withdrawMoneyAction(event.target.dataset.amount))
+            }
+          >
+            WITHDRAW $10,000
+          </button>
+          <button
+            data-amount="5000"
+            onClick={(event) =>
+              store.dispatch(withdrawMoneyAction(event.target.dataset.amount))
+            }
+          >
+            WITHDRAW $5,000
+          </button>
         </section>
 
-        <p className="App__giveaway">Give away all your cash to charity</p>
+        <p
+          className="App__giveaway"
+          onClick={() => store.dispatch(giveAllToCharityAction())}
+        >
+          Give away all your cash to charity
+        </p>
       </div>
     );
   }
