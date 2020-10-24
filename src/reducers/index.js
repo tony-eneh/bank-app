847d2a6101c49571e8c40522e0b87b07e016bbab
Tony Eneh <aeneh90@gmail.com>
2020-10-24 12:07:07 +0100

    Fix: error in my reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,3 @@
-import { withdrawActionCreator } from "../actions";
-
 export const reducer = (store, action) => {
   switch (action.type) {
     case "WITHDRAW_MONEY":
@@ -9,5 +7,11 @@ export const reducer = (store, action) => {
       };
     case "GIVE_ALL_TO_CHARITY":
       return { ...store, totalAmount: 0 };
+    default:
+      // it is important to have a default case and to return the original store in this case.
+      // If you leave out a default case you have errors. Redux probably calls the reducers
+      // before you even dispatch any events. Hence you need a default case which will
+      // just return your store in such circumstances
+      return { ...store };
   }
 };
